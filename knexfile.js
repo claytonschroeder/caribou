66ddc5037ecbb0a8407bd944307a524a695da81f
Clayton Schroeder <clayton.schroeder@alumni.ubc.ca>
2017-08-23 23:44:07 +0000

    configure knex with postgress credentials and references to migration and seed folder.

---
+++ knexfile.js
@@ -0,0 +1,23 @@
+require('dotenv').load();
+
+module.exports = {
+  development: {
+    client: 'postgresql',
+    connection: {
+      database: process.env.DB_NAME,
+      user:     process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: './server/migrations'
+    },
+    seeds: {
+      directory: './server/seeds/'
+    }
+  }
+};

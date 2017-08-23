90300108f66b676e454f35013b1d776f3b10f052
Clayton Schroeder <clayton.schroeder@alumni.ubc.ca>
2017-08-23 23:41:34 +0000

    added a helper library for making API calls to the server from frontend.

---
+++ src/lib/api.js
@@ -0,0 +1,42 @@
+class Api {
+
+  static json(path) {
+    return fetch('/api' + path, {
+      credentials: 'include'
+    }).then(response => {
+      if(!response.ok) {
+        throw Error('could not get /api' + path)
+      }
+      return response
+    }).then(data => data.json());
+  }
+
+  static post(path, data) {
+    // we should really do this all the time and expect objects to be passed in as data.
+    if(typeof data === 'object') {
+      data = JSON.stringify(data);
+    }
+
+    return fetch('/api' + path, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: data,
+      credentials: 'include'
+    }).then(response => response.json());
+  }
+
+  static put(path, data) {
+    return fetch('/api' + path, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data),
+      credentials: 'include'
+    }).then(response => response.json());
+  }
+}
+
+export default Api;

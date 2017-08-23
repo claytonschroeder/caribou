2573cb6127ecf83de6875784ce65a323e0d557fc
Clayton Schroeder <clayton.schroeder@alumni.ubc.ca>
2017-08-23 23:52:47 +0000

    configured projects route, set json body parser limit to 5mb to handle sending base64.

--- server/server.js
+++ server/server.js
@@ -3,13 +3,14 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const path = require('path');
+const db = require('./db');
 const PORT = process.env.PORT || 8001;
 
 
 app.use(express.static('build')); // This is actually a build directory in the parent
-app.use(require('body-parser').json());
+app.use(require('body-parser').json({limit: '5mb'}));
 
-// app.use('/api/characteristics', require('./routes/characteristics')());
+app.use('/api/projects', require('./routes/projects')());
 
 
 app.get('*', function (request, response){

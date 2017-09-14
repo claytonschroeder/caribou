e661c854d18e92e6ddf565ba190d447105e64ca3
Clayton Schroeder <clayton.schroeder@alumni.ubc.ca>
2017-09-14 17:51:09 +0000

    remove material design

--- src/index.jsx
+++ src/index.jsx
@@ -7,11 +7,8 @@ require("./styles/application.scss");
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
-import injectTapEventPlugin from 'react-tap-event-plugin';
 import es6promise from 'es6-promise';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-injectTapEventPlugin();
 es6promise.polyfill();
 
-ReactDOM.render(<MuiThemeProvider><App /></MuiThemeProvider>, document.getElementById('react-root'));
+ReactDOM.render(<App />, document.getElementById('react-root'));
